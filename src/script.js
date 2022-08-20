f06b705639a817bf1f894c2d6df4922aa94cf9b0
Drownie <chaoscrafty01@gmail.com>
2022-08-20 19:43:02 +0700

    add top-bar

--- src/script.js
+++ src/script.js
@@ -1,11 +1,12 @@
-const icons = ["app-store.png", "clock.png", "files.png", "settings.png", "calculator.png"];
+const icons = ["app-store.png", "clock.png", "files.png", "calculator.png", "settings.png"];
 
 const taskbar = document.getElementById("taskbar");
-taskbar.style.width = `${3.5 * icons.length}em`
-taskbar.style.left = `calc(50% - calc(${3.5 * icons.length}em / 2))`
+taskbar.style.width = `${3.85 * icons.length}em`
+taskbar.style.left = `calc(50% - calc(${4 * icons.length}em / 2))`
 
 const app = document.createElement("div");
 app.classList.add("app");
+app.classList.add("rounded");
 
 for (var i = 0; i < icons.length; i++) {
     let appTmp = app.cloneNode();
